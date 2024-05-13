937857b7aac69516efa02c9c3d4ddfb3d174e591
Uma Bisht <anku.uma02@gmail.com>
2024-05-14 02:15:09 +0530

    inProgress

---
+++ expense_tracker/src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <>
+            <form>
+                <div className="row mb-3">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Name</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="inputEmail3" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input type="email" className="form-control" id="inputEmail3" />
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputPassword3" />
+                    </div>
+                </div>
+                
+                
+                <button type="submit" className="btn btn-primary">Sign in</button>
+            </form>
+        </>
+    )
+}
